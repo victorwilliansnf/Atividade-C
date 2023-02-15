fd39170635a52b5fdcbe7c481bd06c2ed7fae09e
victorwilliansnf <119889225+victorwilliansnf@users.noreply.github.com>
2023-02-14 21:05:17 -0400

    Update 20222220004.c

--- 20222220004.c
+++ 20222220004.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, maior, menor;
+    for (int i = 1; i <= 20; i++) {
+        printf("Digite o %dº número: ", i);
+        scanf("%d", &num);
+        if (i == 1) { // se for o primeiro número lido, ele é atribuído tanto à variável maior quanto à variável menor
+            maior = num;
+            menor = num;
+        } else { // se não for o primeiro número lido, ele é comparado com os valores atuais de maior e menor
+            if (num > maior) {
+                maior = num;
+            }
+            if (num < menor) {
+                menor = num;
+            }
+        }
+    }
+    printf("O maior número lido é: %d\n", maior);
+    printf("O menor número lido é: %d\n", menor);
+    return 0;
+}
 
